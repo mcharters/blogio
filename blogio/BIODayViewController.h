a20c827dc8abd46ee22a109d229c29f533048560
Matt Charters <matt@well.ca>
2013-03-06 20:38:11 -0500

    Updated the UI so track names show when selecting days of the week

---
+++ blogio/BIODayViewController.h
@@ -0,0 +1,17 @@
+//
+//  BIODayViewController.h
+//  blogio
+//
+//  Created by Matt Charters on 13-03-06.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@class BIODay;
+
+@interface BIODayViewController : UITableViewController
+
+@property (weak, nonatomic) BIODay *day;
+
+@end
--- blogio/BIOWeekViewController.m
+++ blogio/BIOWeekViewController.m
@@ -10,6 +10,7 @@
 #import "BIODay.h"
 #import "BIOAddFeedViewController.h"
 #import "BIOFeedCollection.h"
+#import "BIODayViewController.h"
 
 @interface BIOWeekViewController () <BIOAddFeedViewControllerDelegate>
 
@@ -24,6 +25,9 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     self.feedCollection = [[BIOFeedCollection alloc] init];
+    
+    // for debug, add stereogum as a feed right away
+    [self.feedCollection addFeedWithURL:@"http://feeds.feedburner.com/stereogum/cBYa?format=xml"];
 }
 
 - (void)viewDidUnload
@@ -61,6 +65,10 @@
     if([segue.identifier isEqualToString:@"AddFeedSegue"]) {
         BIOAddFeedViewController *addController = (BIOAddFeedViewController*)[((UINavigationController*)[segue destinationViewController]) topViewController];
         addController.delegate = self;
+    } else if([segue.identifier isEqualToString:@"ViewDaySegue"]) {
+        BIODayViewController *dayController = ((BIODayViewController*)[segue destinationViewController]);
+        BIODay *selectedDay = [[BIODay weekdays] objectAtIndex:[self.tableView indexPathForSelectedRow].row];
+        dayController.day = selectedDay;
     }
 }
 
