6446760c06c9e32d0c74a2cac24e9032d83652ac
Matt Charters <matt@well.ca>
2013-03-17 14:34:55 -0400

    Download and look for soundcloud links in web source.

--- blogio/BIOFeed.m
+++ blogio/BIOFeed.m
@@ -10,6 +10,8 @@
 #import "MWFeedParser.h"
 #import "BIOTrackCollection.h"
 #import "BIODay.h"
+#import "TFHpple.h"
+#import "TFHppleElement.h"
 
 @interface BIOFeed () <MWFeedParserDelegate>
 
@@ -46,18 +48,35 @@
 - (void)feedParser:(MWFeedParser *)parser didParseFeedInfo:(MWFeedInfo *)info {
 }
 
-- (void)feedParser:(MWFeedParser *)parser didParseFeedItem:(MWFeedItem *)item {    
+- (void)feedParser:(MWFeedParser *)parser didParseFeedItem:(MWFeedItem *)item {
+    NSDateComponents *weekdayComponents = [self.cal components:NSWeekdayCalendarUnit fromDate:item.date];
+    BIODay *day = [[BIODay weekdays] objectAtIndex:[weekdayComponents weekday]];
+    
+    BIOTrackCollection *tracks = [BIOTrackCollection sharedInstance];
+    
     if([item.enclosures count] > 0) {
         // we have some sort of attached file
         // find out what day this file was attached
-        NSDateComponents *weekdayComponents = [self.cal components:NSWeekdayCalendarUnit fromDate:item.date];
-        BIODay *day = [[BIODay weekdays] objectAtIndex:[weekdayComponents weekday]];
-        
-        BIOTrackCollection *tracks = [BIOTrackCollection sharedInstance];
         for (NSDictionary *enclosure in item.enclosures) {
             NSString *trackURL = [enclosure objectForKey:@"url"];
             NSLog(@"Adding track %@ on %@", trackURL, day);
             [tracks addTrack:trackURL forDay:day];
+        }
+    } else {
+        // no enclosure, but maybe there's a stream we can grab.
+        // check the entire post though, it might not be in the summary.
+        NSURL *itemURL = [NSURL URLWithString:item.link];
+        NSData *itemData = [NSData dataWithContentsOfURL:itemURL];
+        
+        TFHpple *post = [[TFHpple alloc] initWithHTMLData:itemData];
+        NSArray *elements = [post searchWithXPathQuery:@"//iframe"];
+        
+        for(TFHppleElement *e in elements) {
+            NSString *src = [e objectForKey:@"src"];
+            if([src rangeOfString:@"soundcloud"].location != NSNotFound) {
+                NSLog(@"Adding track %@ on %@", src, day);
+                [tracks addTrack:src forDay:day];
+            }
         }
     }
 }
