a087dfa7d4c9deeb117ebc817f67cda6ae641c9a
Matt Charters <matt@well.ca>
2013-08-25 17:54:11 -0400

    Download track information from SoundCloud, despite the SoundCloud API documentation's best attempts to thwart me

--- blogio/BIOSoundCloudTrack.m
+++ blogio/BIOSoundCloudTrack.m
@@ -8,9 +8,12 @@
 
 #import "BIOSoundCloudTrack.h"
 
-@interface BIOSoundCloudTrack ()
+@interface BIOSoundCloudTrack () <NSURLConnectionDelegate>
 
 @property (strong, nonatomic) NSString *name;
+@property (strong, nonatomic) NSString *streamURL;
+@property (strong, nonatomic) NSMutableData *receivedData;
+@property (strong, nonatomic) NSURLConnection *soundcloudConnection;
 
 @end
 
@@ -25,7 +28,25 @@
         for(NSString *keyValuePair in urlParams) {
             NSArray *pairComponents = [keyValuePair componentsSeparatedByString:@"="];
             if([[pairComponents objectAtIndex:0] isEqualToString:@"url"]) {
-                _name = [[pairComponents objectAtIndex:1] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+                // get the full track info from the soundcloud API
+                NSURL *trackURL = [NSURL URLWithString:[[pairComponents objectAtIndex:1] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+                NSString *trackID = [trackURL lastPathComponent];
+                
+                NSString *trackInfoURLString = [NSString stringWithFormat:@"http://api.soundcloud.com/tracks/%@.json?client_id=%@", trackID, @"a8e0793f34dc6faa42bcea7831b280d3"];
+                
+                // check for "secret" urls
+                for(NSString *subKeyValuePair in [[trackURL query] componentsSeparatedByString:@"&"]) {
+                    NSArray *subPairComponents = [subKeyValuePair componentsSeparatedByString:@"="];
+                    if([[subPairComponents objectAtIndex:0] isEqualToString:@"secret_token"]) {
+                        trackInfoURLString = [trackInfoURLString stringByAppendingFormat:@"&%@", subKeyValuePair];
+                    }
+                }
+                
+                NSURL *trackInfoURL = [NSURL URLWithString:trackInfoURLString];
+                NSURLRequest *trackInfoRequest = [NSURLRequest requestWithURL:trackInfoURL];
+
+                self.receivedData = [NSMutableData data];
+                self.soundcloudConnection = [NSURLConnection connectionWithRequest:trackInfoRequest delegate:self];
             }
         }
     }
@@ -35,6 +56,39 @@
 
 - (void)play {
     
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+    [self.receivedData setLength:0];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+    [self.receivedData appendData:data];
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+    NSLog(@"Connection failed.");
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+    NSJSONSerialization *json = [NSJSONSerialization JSONObjectWithData:self.receivedData options:0 error:nil];
+    
+    NSString *title;
+    if(json != nil && [json isKindOfClass:[NSDictionary class]]) {
+        NSDictionary *trackInfo = (NSDictionary*)json;
+        
+        title = [trackInfo objectForKey:@"title"];
+        self.streamURL = [trackInfo objectForKey:@"stream_url"];
+    }
+    
+    if(title != nil) {
+        self.name = title;
+    } else {
+        self.name = @"Sorry - track not found";
+    }
+    
+    self.soundcloudConnection = nil;
+    self.receivedData = nil;
 }
 
 @end
